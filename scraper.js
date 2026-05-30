4dc065fb29ac6c81355ef6296bcec6dc820c68a7
Daniel Gutiérrez Adán <danigutiadan4@gmail.com>
2026-05-30 05:21:44 +0200

    feat: implement stealth plugin to bypass datadome blocks

--- scraper.js
+++ scraper.js
@@ -1,4 +1,8 @@
-const { chromium } = require('playwright');
+const { chromium } = require('playwright-extra');
+const stealth = require('puppeteer-extra-plugin-stealth')();
+
+// Aplicar el camuflaje militar
+chromium.use(stealth);
 
 const USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
